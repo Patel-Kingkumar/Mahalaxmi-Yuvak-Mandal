98affa858e8c9612af2fc29c7be817efb15ad93c
Kano409 <kingpatel0409@gmail.com>
2023-12-22 00:24:13 +0530

    fixed header

--- src/app/upcoming/upcoming.component.ts
+++ src/app/upcoming/upcoming.component.ts
@@ -9,9 +9,9 @@ export class UpcomingComponent {
   year: any = new Date().getFullYear();
 
   events: any = [
-    { name: "Salgiri Mohotsav ", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-22-568x492.jpg", text: "this is a", date: "04/09/2023" },
-    { name: "Ganesh Mohotsav", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-23-568x492.jpg", text: "this is b", date: "05/09/2023" },
-    { name: "Navratri Mohotsav", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-25-568x492.jpg", text: "this is c", date: "06/09/2023" },
+    { name: "Salgiri Mahotsav ", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-22-568x492.jpg", text: "this is a", date: "04/09/2023" },
+    { name: "Ganesh Mahotsav", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-23-568x492.jpg", text: "this is b", date: "05/09/2023" },
+    { name: "Navratri Mahotsav", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-25-568x492.jpg", text: "this is c", date: "06/09/2023" },
     { name: "Dahnu Padyatra", image: "https://livedemo00.template-help.com/wt_prod-19077/images/post-25-568x492.jpg", text: "this is d", date: "07/09/2023" },
   ]
 
