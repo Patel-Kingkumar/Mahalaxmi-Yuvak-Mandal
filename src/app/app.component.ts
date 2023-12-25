ced1e6231f428858ccbed4e0f4dd64d8cee9a670
Kano409 <kingpatel0409@gmail.com>
2023-12-26 00:07:57 +0530

    svroll btn position set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-import { scan, takeWhile, tap } from 'rxjs';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 
 @Component({
@@ -8,11 +7,22 @@ import { scan, takeWhile, tap } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'website';
+  displayDiv: boolean = false;
 
   constructor(@Inject(DOCUMENT) private dom: Document) { }
+  ngOnInit(): void {
 
+  }
+  @HostListener("window:scroll", [])onWindowScroll() {
+    let windowHeight = this.dom.documentElement.scrollTop;
+    if(windowHeight > 500) {
+      this.displayDiv = true;
+    } else {
+      this.displayDiv = false;
+    }
+ }
   scrollToTop() {
     this.dom.body.scrollTop = 0;
     this.dom.documentElement.scrollTop = 0;
