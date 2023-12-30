d4307e98d5bc6323a87df21a49e991c015d340b9
Kano409 <kingpatel0409@gmail.com>
2023-12-31 00:15:43 +0530

    change carousel video

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
-  heading: any = ["Identity", "Founders", "Missions",];
+  heading: any = ["Identity", "Founders", "Missions"];
+  text: any = "Mahalaxmi Yuvak Mandal is one of most popular group in Dumas. We educate people about sanatan dharma."
   description: any = [
     "We are one of the leading Hinduism websites on the Web offering you both essential and detailed information as well as news from the world of this religion.",
     "Our organization was established by some of the most prominent persons in the Hindu world. They were and still are the core people of our organization and the website itself.",
     "Our mission is to help everyone interested in Hinduism discover more about this religion and its community. We aim to make it clear for our every visitor what Hinduism is like."
   ]
-  text: any = this.description[0];
 
   constructor() { }
 
